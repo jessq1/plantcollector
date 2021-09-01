438ab1bd8663a0c78f23130a2c8764f586a2f7d1
JessQi <85958041+jessq1@users.noreply.github.com>
2021-09-01 15:43:40 -0400

    part 3 done

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Plant
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 
 # Create your views here.
 def home(request):
@@ -15,3 +17,17 @@ def plants_index(request):
 def plants_detail(request, plant_id):
   plant = Plant.objects.get(id=plant_id)
   return render(request, 'plants/detail.html', { 'plant': plant })
+
+class PlantCreate(CreateView):
+  model = Plant
+  fields = '__all__'
+  success_url = '/plants/'
+
+class PlantUpdate(UpdateView):
+  model = Plant
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['origin', 'description', 'age']
+
+class PlantDelete(DeleteView):
+  model = Plant
+  success_url = '/plants/'
